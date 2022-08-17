c7c1e799e9189cc5699996cbc2259a5bbecf0895
Claire LESAFFRE <clesaffr@e1r8p1.clusters.42paris.fr>
2022-08-17 22:10:25 +0200

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,29 @@
 /*   By: clesaffr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 07:58:12 by clesaffr          #+#    #+#             */
-/*   Updated: 2022/08/09 22:06:33 by clesaffr         ###   ########.fr       */
+/*   Updated: 2022/08/12 04:54:23 by clesaffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-#include "../minilibx-linux/mlx.h"
+
+int	frame(void *mlx_win)
+{
+	int		*bits_per_pixel;
+	int		*size_line;
+	int		*endian;
+	char 	*str;
+	void	*mlx_image;
+	int		i;
+
+	i = 0;
+	str = NULL;
+	while (i++ < 10)
+	{
+		mlx_image = mlx_new_image(mlx_win, 1980, 1080);
+		str = mlx_get_data_addr(mlx_image, bits_per_pixel, size_line, endian);
+	}
+}
 
 int	main(void)
 {
@@ -21,4 +38,6 @@ int	main(void)
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	mlx_loop(mlx);
+	mlx_destroy_window(mlx, mlx_win);
+	return (0);
 }
